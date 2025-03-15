d3a2e5e1f40f40025f86f4312f7d09fccac5aad0
Max <70074382+maxdurov@users.noreply.github.com>
2025-03-15 23:45:39 +0300

    fix 1.0.1

--- src/main/java/org/webproxy/server/ServerProxyHandler.java
+++ src/main/java/org/webproxy/server/ServerProxyHandler.java
@@ -7,6 +7,8 @@ import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
 import java.util.Arrays;
+import java.util.OptionalInt;
+import java.util.stream.IntStream;
 
 public class ServerProxyHandler {
 
@@ -16,19 +18,30 @@ public class ServerProxyHandler {
 
     public ServerProxyHandler(SocketChannel clientSocket) throws IOException, InterruptedException {
         this.clientSocket = clientSocket;
+
         this.clientSocket.configureBlocking(false);
         requestHandler();
     }
 
     public void requestHandler() throws IOException, InterruptedException {
-        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        ByteBuffer buffer = ByteBuffer.allocate(1024 * 5);
         int bytesRead;
 
+        int wait = 0;
+
         while (true) {
-            buffer.clear();
 
             bytesRead = clientSocket.read(buffer);
+            if (buffer.position() >= 4){
+                if (buffer.get(buffer.position()-1) == 10 & buffer.get(buffer.position()-2) == 13 & buffer.get(buffer.position()-3) == 10 & buffer.get(buffer.position()-4) == 13){
+                    break;
+                }
+            }
 
+            if (wait > 5000) break;
+            wait = wait+1;
+            Thread.sleep(1);
+        }
             if (bytesRead > 0) {
                 buffer.flip();
                 String resp = new String(buffer.array());
@@ -36,24 +49,29 @@ public class ServerProxyHandler {
 
                 String[] header = resp.split("\r\n");
 
-                //PROTOCOL.debugOutPut(Arrays.toString(header));
+                PROTOCOL.debugOutPut(Arrays.toString(header));
 
                 if (header[0].startsWith("CONNECT")) {
 
                     PROTOCOL.debugOutPut("DETECT CONNECT");
                     String[] addr = (header[0].split(" ")[1]).replace(" ", "").split(":");
 
-
                     if (connectToSite(new InetSocketAddress(addr[0], Integer.parseInt(addr[1])))) {
                         clientSocket.write(ByteBuffer.wrap("HTTP/1.1 200 Connection Established\r\n\r\n".getBytes()));
+
                         connectToSiteTunnel();
                     } else {
                         clientSocket.close();
                     }
-                } else if (header[1].toUpperCase().startsWith("HOST")) {
+                } else if (Arrays.stream(header).anyMatch(s -> s.toUpperCase().contains("HOST"))) {
                     PROTOCOL.debugOutPut("DETECT");
-                    System.out.println(header[0]);
-                    String[] addr = header[1].split(":");
+
+                    int index = (IntStream.range(0, header.length)
+                            .filter(i -> header[i].toUpperCase().contains("HOST"))
+                            .findFirst()).getAsInt();
+
+                    String[] addr = header[index].split(":");
+
                     InetSocketAddress address;
                     if (addr.length < 3) {
                         address = new InetSocketAddress(addr[1].replace(" ", ""), 80);
@@ -69,11 +87,10 @@ public class ServerProxyHandler {
                     System.out.println("ERR METHOD: " + Arrays.toString(header));
                 }
 
-                clientSocket.close();
-                break;
-            }
-            Thread.sleep(1);
+
+
         }
+        clientSocket.close();
     }
 
 
@@ -104,7 +121,7 @@ public class ServerProxyHandler {
             try {
 
                 siteBytesRead = siteSocket.read(site_buffer);
-                Thread.sleep(1);
+
                 clientBytesRead = clientSocket.read(client_buffer);
 
                 if (siteBytesRead == -1) break;
@@ -129,6 +146,7 @@ public class ServerProxyHandler {
                     }
                     client_buffer.clear();
                 }
+                Thread.sleep(1);
             } catch (Exception e) {
                 break;
             }
